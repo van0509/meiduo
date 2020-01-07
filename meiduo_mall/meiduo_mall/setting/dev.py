6bc22e9cf77cd049eb2c98773aae0e397d665b19
Seven <free001@vip.qq.com>
2020-01-07 23:12:27 +0800

    修改部分代码 添加图片

--- meiduo_mall/meiduo_mall/setting/dev.py
+++ meiduo_mall/meiduo_mall/setting/dev.py
@@ -12,10 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import sys
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print(os.path.abspath(__file__))
 # sys.path保存了python解析器的导包路径
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
